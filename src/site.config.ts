c6f98b4de2c2f17afc28a35039dd551a249f45e4
Ishan Brar <ishanbrar@hotmail.com>
2025-04-02 03:27:03 -0500

    Added more text and pages

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Ishan Brar",
+	author: "JJS",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en-GB",
@@ -33,11 +33,11 @@ export const menuLinks: { path: string; title: string }[] = [
 		title: "About",
 	},
 	{
-		path: "/posts/",
-		title: "Blog",
+		path: "/why/",
+		title: "Why",
 	},
 	{
-		path: "/notes/",
-		title: "Notes",
+		path: "/how/",
+		title: "How",
 	},
 ];
